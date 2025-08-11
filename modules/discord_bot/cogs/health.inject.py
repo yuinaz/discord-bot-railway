479e7842e97d521093a879e24744f9c88be456b3
unknown <CC-PC@ACT-CC.horisonultimahotels.com>
2025-08-11 14:14:58 +0700

    chore: remove stray *.inject.py patch files

--- modules/discord_bot/cogs/health.inject.py
+++
@@ -1,6 +0,0 @@
-# --- injected override: force log channel by ID ---
-try:
-    from ..helpers import log_utils as _lu
-    _lu.LOG_CHANNEL_ID = int("1400375184048787566")
-except Exception:
-    pass
--- modules/discord_bot/events/bot_events.inject.py
+++
@@ -1,6 +0,0 @@
-# --- injected override: force log channel by ID ---
-try:
-    from modules.discord_bot.helpers import log_utils as _lu
-    _lu.LOG_CHANNEL_ID = int("1400375184048787566")
-except Exception:
-    pass
