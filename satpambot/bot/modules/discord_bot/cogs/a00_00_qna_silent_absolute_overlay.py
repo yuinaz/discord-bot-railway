1da076f2bc9bbc326a894fc1af2fff40aa2a6659
Namamu <emailmu@contoh.com>
2025-10-29 23:57:42 +0700

    fix

--- satpambot/bot/modules/discord_bot/cogs/a00_00_qna_silent_absolute_overlay.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from discord.ext import commands
-class _Noop(commands.Cog):
-    """noop"""
-    def __init__(self, bot): self.bot = bot
-async def setup(bot: commands.Bot):
-    await bot.add_cog(_Noop(bot))
